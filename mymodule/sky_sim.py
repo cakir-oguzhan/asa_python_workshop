5493a12c8bfdf23fdeca9f2179f4bc7ff84def0e
Oguzhan Cakir <oguzhancakir@Oguzhans-MacBook-Pro.local>
2023-09-13 15:02:19 +1000

    Updated log messages

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -13,6 +13,28 @@ NSRC = 1_000
 RA = '00:42:44.3'
 DEC = '41:16:09'
 
+def skysim_parser():
+    """
+    Configure the argparse for skysim
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        The parser for skysim.
+    """
+    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-', description="Simulate a sky")
+    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
+                        help="Central ra (degrees) for the simulation location")
+    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
+                        help="Central dec (degrees) for the simulation location")
+    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
+                        help='destination for the output catalog')
+    parser.add_argument('--logging', type=str, default="INFO",
+                        help='Logging types (INFO, DEBUG, WARNING, ERROR, CRITICAL)')
+    return parser
+
+
+
 def get_radec():
     """
     Generate the ra/dec coordinates of Andromeda
@@ -37,40 +59,6 @@ def get_radec():
     ra = ra/cos(dec*pi/180)
     return ra,dec
 
-def clip_to_radius(ra, dec, ras, decs):
-    output_ras = []
-    output_decs = []
-    for ra_i, dec_i in zip(ras, decs):
-        if ra_i**2 + dec_i**2 < 1:
-            output_ras.append(ra_i)
-            output_decs.append(dec_i)
-    return output_ras, output_decs
-
-
-def generate_sky_pos():
-    # from wikipedia
-    ra = '00:42:44.3'
-    dec = '41:16:09'
-
-    # convert to decimal degrees
-
-    d, m, s = dec.split(':')
-    dec = int(d)+int(m)/60+float(s)/3600
-
-    h, m, s = ra.split(':')
-    ra = 15*(int(h)+int(m)/60+float(s)/3600)
-    ra = ra/cos(dec*pi/180)
-
-
-    # make 1000 stars within 1 degree of Andromeda
-    ras = []
-    decs = []
-    for i in range(NSRC):
-        ras.append(ra + uniform(-1,1))
-        decs.append(dec + uniform(-1,1))
-    return ras, decs
-
-
 def make_stars(ra, dec, nsrc=NSRC):
     """
     Generate NSRC stars within 1 degree of the given ra/dec
@@ -89,31 +77,27 @@ def make_stars(ra, dec, nsrc=NSRC):
     """
     ras = []
     decs = []
+    
     for _ in range(nsrc):
         ras.append(ra + uniform(-1,1))
         decs.append(dec + uniform(-1,1))
     return ras, decs
 
 
-def skysim_parser():
-    """
-    Configure the argparse for skysim
+def clip_to_radius(ra, dec, ras, decs): #, log=logging.getLogger("<my module>")
+    output_ras = []
+    output_decs = []
 
-    Returns
-    -------
-    parser : argparse.ArgumentParser
-        The parser for skysim.
-    """
-    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-', description="Simulate a sky")
-    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
-                        help="Central ra (degrees) for the simulation location")
-    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
-                        help="Central dec (degrees) for the simulation location")
-    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
-                        help='destination for the output catalog')
-    parser.add_argument('--logging', type=str, default="INFO",
-                        help='Logging types (INFO, DEBUG, WARNING, ERROR, CRITICAL)')
-    return parser
+    log.info(f'Now simulating {NSRC} stars within the square centered at the given coordinates!')
+
+    log.info(f"Let's exclude those not within 1 degree circle!")
+    for ra_i, dec_i in zip(ras, decs):
+        if ra_i**2 + dec_i**2 < 1:
+            output_ras.append(ra_i)
+            output_decs.append(dec_i)
+    
+    log.info(f'There are {len(output_ras)} stars within 1 degree around the given coordinates!')
+    return output_ras, output_decs
 
 
 def main():
@@ -147,9 +131,8 @@ def main():
 
     log.info(f'Central RA - DEC = {ra:.3f} - {dec:.3f}')
     
-
-    log.info(f'Now simulating {NSRC} stars around the central coordinates!')
     ras, decs = make_stars(ra,dec)
+    ras, decs = clip_to_radius(ra_deg, dec_deg, ras, decs, log=log)
 
     # now write these to a csv file for use by my other program
     with open(options.out,'w') as f:
